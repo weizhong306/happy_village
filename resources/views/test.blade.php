b0ffd348aed8ef96c1781c54aca75b740e3be7ba
huangweizhong <yo577306@gmail.com>
2024-02-22 15:17:35 +0800

    feat: 建立jenkins測試畫面

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <h1>Jenkins Test</h1>
+</body>
+</html>
